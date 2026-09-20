7b0ead5c10e8eabaa80daa32ce106ef3ee217bb0
sthm23 <stuxtamixhev@gmail.com>
2026-09-20 20:05:26 +0500

    feat: refactor AddTransactionSheet to use BottomSheet component for improved UI experience

--- src/screens/payment/AddTransactionSheet.tsx
+++ src/screens/payment/AddTransactionSheet.tsx
@@ -3,17 +3,18 @@ import { Text } from "@/components/ui/text";
 import { HStack } from "@/components/ui/hstack";
 import { Divider } from "@/components/ui/divider";
 import {
-  Actionsheet,
-  ActionsheetBackdrop,
-  ActionsheetContent,
-  ActionsheetDragIndicator,
-  ActionsheetDragIndicatorWrapper,
-} from "@/components/ui/actionsheet";
+  BottomSheet,
+  BottomSheetPortal,
+  BottomSheetBackdrop,
+  BottomSheetScrollView,
+  type BottomSheetRef,
+} from "@/components/ui/bottomsheet";
 import CustomIcon from "@/icons/custom-icon";
 import { IconNames } from "@/icons/icon.type";
 import { CashTransactionType } from "@/models/payment.model";
 import { CreateTransactionPayload } from "@/services/cashbox/cashbox.types";
 import { useTranslation } from "react-i18next";
+import { useEffect, useRef } from "react";
 import { TransactionForm } from "./TransactionForm";
 
 type Props = {
@@ -34,6 +35,15 @@ export const AddTransactionSheet = ({
   isLoading = false,
 }: Props) => {
   const { t } = useTranslation();
+  const sheetRef = useRef<BottomSheetRef>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      sheetRef.current?.open();
+    } else {
+      sheetRef.current?.close();
+    }
+  }, [isOpen]);
 
   const isIncome = transactionType === CashTransactionType.INCOME;
   const iconName = isIncome ? IconNames.ARROW_UP : IconNames.ARROW_DOWN;
@@ -42,34 +52,32 @@ export const AddTransactionSheet = ({
   const typeLabel = isIncome ? t('payment.type.INCOME') : t('payment.type.EXPENSE');
 
   return (
-    <Actionsheet isOpen={isOpen} onClose={onClose}>
-      <ActionsheetBackdrop />
-      <ActionsheetContent className="max-h-[90vh]">
-        <ActionsheetDragIndicatorWrapper>
-          <ActionsheetDragIndicator />
-        </ActionsheetDragIndicatorWrapper>
+    <BottomSheet ref={sheetRef} onClose={onClose}>
+      <BottomSheetPortal
+        snapPoints={['85%']}
+        backdropComponent={(props) => <BottomSheetBackdrop {...props} />}
+      >
+        <BottomSheetScrollView>
+          <Box className="px-4 pt-2 pb-6">
+            <HStack className="items-center gap-3 mb-4">
+              <Box className={`h-10 w-10 items-center justify-center rounded-full ${iconBgColor}`}>
+                <CustomIcon name={iconName} size={18} color={iconColor} />
+              </Box>
+              <Text size="2xl" bold>{typeLabel}</Text>
+            </HStack>
 
-        <Box className="w-full px-4 pt-4 pb-6">
-          {/* Header */}
-          <HStack className="items-center gap-3 mb-4">
-            <Box className={`h-10 w-10 items-center justify-center rounded-full ${iconBgColor}`}>
-              <CustomIcon name={iconName} size={18} color={iconColor} />
-            </Box>
-            <Text size="2xl" bold>{typeLabel}</Text>
-          </HStack>
+            <Divider className="mb-5" />
 
-          <Divider className="mb-5" />
-
-          {/* Form */}
-          <TransactionForm
-            cashboxId={cashboxId}
-            transactionType={transactionType}
-            onSubmit={onSubmit}
-            onCancel={onClose}
-            isLoading={isLoading}
-          />
-        </Box>
-      </ActionsheetContent>
-    </Actionsheet>
+            <TransactionForm
+              cashboxId={cashboxId}
+              transactionType={transactionType}
+              onSubmit={onSubmit}
+              onCancel={onClose}
+              isLoading={isLoading}
+            />
+          </Box>
+        </BottomSheetScrollView>
+      </BottomSheetPortal>
+    </BottomSheet>
   );
 };
